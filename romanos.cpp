ad192770a704ebaac8581838fb2da763022d1430
daves22 <davisilvamatias@gmail.com>
2024-04-27 22:05:17 -0500

    teste 1 - nomeação correta dos algarismos romanos base

--- romanos.cpp
+++ romanos.cpp
@@ -1,6 +1,40 @@
+#include <unordered_map>
 #include "romanos.hpp"
+
 int romanos_para_decimal(char const * num_romano)
 {
-  return 0; 
+  int decimalValue = 0;
+
+  // Verifica cada algarismo romano de maneira singular
+  for(int i = 0; num_romano[i] != '\0'; i++){  
+    // atribui o valor decimal ao caractere atual
+    switch (num_romano[i]) {
+      case 'I':
+        decimalValue += 1;
+        break;
+      case 'V':
+        decimalValue += 5;
+        break;
+      case 'X':
+          decimalValue += 10;
+          break;
+      case 'L':
+          decimalValue += 50;
+          break;
+      case 'C':
+          decimalValue += 100;
+          break;
+      case 'D':
+          decimalValue += 500;
+          break;
+      case 'M':
+          decimalValue += 1000;
+          break;
+      default:
+            // Se o algarismo romano não for válido, retorna -1
+          decimalValue = -1;
+      }
+    }
+  return decimalValue;
 }
  
--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -9,14 +9,59 @@ TEST_CASE( "Numeros romanos - algarismos �nicos", "[romanos]" ) {
     
     REQUIRE( romanos_para_decimal("V") == 5 );
 
-    
+    REQUIRE( romanos_para_decimal("X") == 10 );
+
+    REQUIRE( romanos_para_decimal("L") == 50 );
+
+    REQUIRE( romanos_para_decimal("C") == 100 );
+
+    REQUIRE( romanos_para_decimal("D") == 500 );
+
+    REQUIRE( romanos_para_decimal("M") == 1000 );
+
 }
 
 
 
 
 TEST_CASE( "Numeros romanos - algarismos inv�lidos", "[romanos]" ) {
+    REQUIRE( romanos_para_decimal("A") == -1 );
+
+    REQUIRE( romanos_para_decimal("B") == -1 );
+
+    REQUIRE( romanos_para_decimal("E") == -1 );
+
+    REQUIRE( romanos_para_decimal("F") == -1 );
+
     REQUIRE( romanos_para_decimal("G") == -1 );
+
+    REQUIRE( romanos_para_decimal("H") == -1 );
+
+    REQUIRE( romanos_para_decimal("J") == -1 );
+
+    REQUIRE( romanos_para_decimal("K") == -1 );
+
+    REQUIRE( romanos_para_decimal("N") == -1 );
+
+    REQUIRE( romanos_para_decimal("O") == -1 );
+
+    REQUIRE( romanos_para_decimal("P") == -1 );
+
+    REQUIRE( romanos_para_decimal("Q") == -1 );
+
+    REQUIRE( romanos_para_decimal("R") == -1 );
+
+    REQUIRE( romanos_para_decimal("S") == -1 );
+
+    REQUIRE( romanos_para_decimal("T") == -1 );
+
+    REQUIRE( romanos_para_decimal("U") == -1 );
+
+    REQUIRE( romanos_para_decimal("W") == -1 );
+
+    REQUIRE( romanos_para_decimal("Y") == -1 );
+    
+    REQUIRE( romanos_para_decimal("Z") == -1 );
     
     REQUIRE( romanos_para_decimal("i") == -1 );
 
