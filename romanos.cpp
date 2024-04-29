2d9e9af1ac8e1889f3f1fa4ade4285c0f6710e04
Davi Silva <davisilvamatias@gmail.com>
2024-04-29 16:05:31 -0300

    Teste 4: teste para verificar se as operações estao certas, L e V com erro.

--- romanos.cpp
+++ romanos.cpp
@@ -13,38 +13,37 @@ int romanos_para_decimal(char const * num_romano)
     // atribui o valor decimal ao caractere atual
     switch (num_romano[i]) {
       case 'I':
-        decimalValue = 1;
+        decimalValue += 1;
         break;
       case 'V':
         if(prev_symbol == 'I'){
-          decimalValue -= 1; //Subtrai 1
-        }else if (prev_symbol == 'V'){
-          decimalValue = -1;  // Se tiver dois repetidos ja alega erro 
+          decimalValue += 3; //Subtrai 2 por conta do +1 do I
         }else{
           decimalValue += 5;
         }
         break;
       case 'X':
         if (prev_symbol == 'I'){
-          decimalValue += 9; //Subtrai 1
+          decimalValue += 8; //Subtrai 2 por conta do +1 do I
         }else{
           decimalValue += 10;
         }
         break;  
       case 'L':
         if (prev_symbol == 'X'){
-          decimalValue += 40; //Subtrai 10
-        }else if(prev_symbol == 'I' || prev_symbol == 'V'){
-          decimalValue = -1;
+          decimalValue += 30; //Subtrai 20 pois tem o 10 do X
         }else{
-          decimalValue += 50;
-        } 
+          decimalValue +=50;
+        }
+        if(prev_symbol == 'I' || prev_symbol == 'V'){
+          decimalValue = -1;
+        }
         break;
       case 'C':
         if(prev_symbol == 'L' || prev_symbol == 'I' || prev_symbol == 'V'){ // se precedido por L, V ou I da erro
           decimalValue = -1;
         }else if (prev_symbol == 'X'){  //Subtrai 10
-          decimalValue += 90;
+          decimalValue += 80; // Soma 80 pois tem o +10 do X
         }else{
           decimalValue += 100;
         }  
@@ -52,8 +51,8 @@ int romanos_para_decimal(char const * num_romano)
       case 'D':
         if (prev_symbol == 'I' || prev_symbol == 'V' || prev_symbol == 'X'){
           decimalValue = -1;
-        }else if (prev_symbol == 'C'){ // Subtrai 100
-          decimalValue += 400;
+        }else if (prev_symbol == 'C'){ // Subtrai 200
+          decimalValue += 300;
         }else{
           decimalValue += 500;
         }
@@ -62,7 +61,7 @@ int romanos_para_decimal(char const * num_romano)
         if (prev_symbol == 'I' || prev_symbol == 'V' || prev_symbol == 'X' || prev_symbol == 'L' || prev_symbol == 'D'){
           decimalValue = -1;
         }else if (prev_symbol == 'C'){ //Subtrai 100
-          decimalValue += 900;  
+          decimalValue += 800;  
         }else{
           decimalValue += 1000;
         }
@@ -72,14 +71,17 @@ int romanos_para_decimal(char const * num_romano)
           decimalValue = -1;
     }
     //verifica se há mais de tres simbolos repetidos consecutivamente
-    if(num_romano[i] == prev_symbol){
+    if (num_romano[i] == prev_symbol) {
+      if (prev_symbol == 'V' || prev_symbol == 'L') {
+        return -1; // V e L não podem ser repetidos consecutivamente
+      }
       contador++;
-      if(contador > 3) return -1; //Mais de tres simbolos retorna -1
-    } else {
-      prev_symbol = num_romano[i];
-      contador = 1;
+      if (contador > 3) return -1; // Mais de três símbolos retorna -1
+      }else {
+        prev_symbol = num_romano[i];
+        contador = 1;
+      }
     }
-  }
   return decimalValue;
 }
  
